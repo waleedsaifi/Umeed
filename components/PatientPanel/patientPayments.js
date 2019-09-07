e28e289d2bbd1162e096fc14f1db244279fe2e0e
Waleed <hafizwaleed0890@gmail.com>
2019-09-07 15:47:03 +0500

    Modified Code Structure

--- components/PatientPanel/patientPayments.js
+++ components/PatientPanel/patientPayments.js
@@ -1,30 +1,26 @@
-import React,{Component} from 'react';
-import {View , Text , StyleSheet} from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 
 
-class patientPayments extends Component{
-render(){
-    const personInffo = this.props.navigation.getParam('personInfo')
-return(
-
-<View style = {styles.container}>
-
-<Text>Payment module is under construction</Text>
-<Text> {personInffo.name} </Text>
-<Text> {personInffo.contact} </Text>
-   
-</View>
-
-);
-}
-}export default  patientPayments;
+class patientPayments extends Component {
+    render() {
+        const personInffo = this.props.navigation.getParam('personInfo')
+        return (
+            <View style={styles.container}>
+                <Text>Payment module is under construction</Text>
+                <Text> {personInffo.name} </Text>
+                <Text> {personInffo.contact} </Text>
+            </View>
+        );
+    }
+} export default patientPayments;
 
 const styles = StyleSheet.create({
 
-    container:{
-        flex:1,
-        justifyContent:'center',
-        alignItems:'center'
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
     }
 
 
--- components/settingScreen.js
+++ components/settingScreen.js
@@ -1,24 +1,17 @@
 import React from 'react';
 import { StyleSheet, Text, View, Image,ImageBackground} from 'react-native';
-import {Header , Left, Right, Icon, Container} from 'native-base'
-
 
 class settingScreen extends React.Component {
 
-
-
     render(){
       return(
-      
             <View  style={styles.container}>
             <Text>Hello from Admin panel</Text>
             </View> 
-
       );
     }  
   }
   export default settingScreen;
-
 
   const styles = StyleSheet.create({
   container: {
